c17d408e2f54bd722ce58008a6af39fcc42c471f
Aazim Sadan <aazimsadan99@gmail.com>
2024-09-10 16:39:11 +0530

    redux toolkit project

--- reduxToolkitTodo/src/features/todo/todoSlice.js
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -1,8 +1,10 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import {createSlice, nanoid } from '@reduxjs/toolkit';
 
 const initialState = {
-    todos: [{id:1, text: "Hello World"}]
+    todos: [{id: 1, text: "Hello world"}]
 }
+
+
 
 export const todoSlice = createSlice({
     name: 'todo',
@@ -16,12 +18,9 @@ export const todoSlice = createSlice({
             state.todos.push(todo)
         },
         removeTodo: (state, action) => {
-            state.todos = state.todos.filter((todo) => 
-                todo.id !== action.payload
-            )
-        }
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload )
+        },
     }
-
 })
 
 export const {addTodo, removeTodo} = todoSlice.actions
