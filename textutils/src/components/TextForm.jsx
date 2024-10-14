5e3b31b410264c0b3c0835b85a8940e6d9544ddf
Aazim Sadan <aazimsadan99@gmail.com>
2024-10-14 23:38:52 +0530

    counting words and characters

--- textutils/src/components/TextForm.jsx
+++ textutils/src/components/TextForm.jsx
@@ -7,6 +7,11 @@ function TextForm(props) {
         setText(newText)
     }
 
+    const handleLoClick = () =>{
+        let newText = text.toLowerCase()
+        setText(newText)
+    }
+
     const handleOnChange = (event)=>{
         setText(event.target.value)
         
@@ -17,14 +22,23 @@ function TextForm(props) {
     const [text, setText] = useState('Enter text here')
     
     return (
-        <div>
+        <>
+        <div className='container'>
             <h2>{props.heading}</h2>
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="6"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-
+            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
         </div>
+        <div className="container my-3">
+            <h2>Your text summary</h2>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
 
