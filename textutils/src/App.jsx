f0b65eba1bd9e1558d1f5c8e44716c8ee492f0ab
Aazim Sadan <aazimsadan99@gmail.com>
2024-11-06 01:25:27 +0530

    suto dismissing alert message

--- textutils/src/App.jsx
+++ textutils/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import About from './components/About'
+import Alert from './components/Alert'
 import Navbar from './components/Navbar'
 import TextForm from './components/TextForm'
 import { useState } from 'react'
@@ -7,21 +8,36 @@ import { useState } from 'react'
 
 function App() {
   const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
 
- const toggleMode = () => {
-   if (mode === 'light') {
-     setMode ('dark')
-     document.body.style.backgroundColor = "#243b5c"
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = "#243b5c"
+      showAlert("Dark mode has been enbale", "Success")
     } else {
       setMode('light')
       document.body.style.backgroundColor = "white"
+      showAlert("Light mode has been enbale", "Success")
     }
   }
   return (
     <>
       <Navbar title="TextUtils" aboutText=" About TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container">
-        <TextForm heading="Enter the to analyze"  mode={mode}/>
+        <TextForm showAlert={showAlert} heading="Enter the to analyze" mode={mode} />
         <About mode={mode} />
       </div>
     </>
